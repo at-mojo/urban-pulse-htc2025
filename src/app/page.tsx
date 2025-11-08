7cd875fdab91558b7caf9dbac0a79cb1aca50593
Mohammad Al-Ahdal <mo@mohammad.dev>
2025-11-08 13:41:26 -0700

    refactor: replace Map component with GlMap in Home component for improved map integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 "use client";
 
-import Image from "next/image";
-import Map from 'react-map-gl/mapbox';
+import GlMap from 'react-map-gl/mapbox';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 export default function Home() {
   return (
     <main className="flex flex-1 flex-col items-center justify-center p-24">
-      <Map
+      <GlMap
       mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
       initialViewState={{
         longitude: -122.4,
