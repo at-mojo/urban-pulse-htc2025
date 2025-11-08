4c92b5ed571e2d95f2d2641b2f25d3e422c1f5c0
Mohammad Al-Ahdal <mo@mohammad.dev>
2025-11-08 14:27:23 -0700

    update location to yyc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,9 @@ export default function Home() {
       <GlMap
       mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
       initialViewState={{
-        longitude: -122.4,
-        latitude: 37.8,
-        zoom: 14
+        longitude: -114.0719,
+        latitude: 51.0447,
+        zoom: 11
       }}
       style={{width: "100%", height: "100%", position: "absolute", top: 0, left: 0, zIndex: 0}}
       mapStyle="mapbox://styles/mapbox/dark-v11"
