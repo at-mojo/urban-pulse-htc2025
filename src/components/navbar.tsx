24363892097b2ad51343e259eca162d999cfee3e
Mohammad Al-Ahdal <mo@mohammad.dev>
2025-11-08 14:26:03 -0700

    feat: update NavBar to display UserAvatar for authentication links

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,7 +6,7 @@ import { Button } from "./ui/button";
 import { cn } from "@/lib/util";
 import { stackClientApp } from "@/stack/client";
 import { useState, useEffect } from "react";
-import type { CurrentUser } from "@stackframe/stack";
+import { UserAvatar, type CurrentUser } from "@stackframe/stack";
 export const NavBar = () => {
   const [user, setUser] = useState<CurrentUser | undefined>(undefined);
   useEffect(() => {
@@ -27,12 +27,12 @@ export const NavBar = () => {
     {
       show: !isLoggedIn,
       href: "/handler/sign-up",
-      label: "Sign Up",
+      label: <UserAvatar user={user as CurrentUser} />,
     },
     {
       show: isLoggedIn,
       href: "/handler/sign-out",
-      label: "Sign Out",
+      label: <UserAvatar user={user as CurrentUser} />,
     },
   ];
   return (
@@ -42,23 +42,26 @@ export const NavBar = () => {
         <span className="text-2xl text-foreground/50">City of Calgary</span>
       </h1>
       <div className="flex flex-row items-center justify-center gap-4 pr-4">
-        {links.map(
-          (link) =>
-            link.show && (
-              <Button variant="ghost" size="lg" asChild key={link.href}>
-                <Link
-                  href={link.href}
-                  className={cn(
-                    pathname === link.href
-                      ? "text-primary"
-                      : "text-foreground/50",
-                    "hover:text-primary"
-                  )}
-                >
-                  {link.label}
-                </Link>
-              </Button>
-            )
+        {links.map((link) =>
+          link.show && (typeof link.label === "string" ? (
+            <Button variant="ghost" size="lg" asChild key={link.href}>
+              <Link
+                href={link.href}
+                className={cn(
+                  pathname === link.href
+                    ? "text-primary"
+                    : "text-foreground/50",
+                  "hover:text-primary"
+                )}
+              >
+                {link.label}
+              </Link>
+            </Button>
+          ) : (
+            <Link href={link.href} key={link.href}>
+              {link.label}
+            </Link>
+          ))
         )}
       </div>
     </nav>
