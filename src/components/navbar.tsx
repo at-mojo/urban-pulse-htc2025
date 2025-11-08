f0e401d56b7fa6ca2b8825ea6beb010a775e1bd2
Mohammad Al-Ahdal <mo@mohammad.dev>
2025-11-08 14:29:30 -0700

    refactor: simplify NavBar component by removing hardcoded Home link and wrapping title in Link for better navigation

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -20,11 +20,6 @@ export const NavBar = () => {
 
   const links = [
     {
-      show: true,
-      href: "/",
-      label: "Home",
-    },
-    {
       show: !isLoggedIn,
       href: "/handler/sign-up",
       label: <UserAvatar user={user as CurrentUser} />,
@@ -37,31 +32,35 @@ export const NavBar = () => {
   ];
   return (
     <nav className="fixed top-0 left-0 w-full bg-background/50 backdrop-blur-lg z-50 flex flex-row items-center justify-between">
-      <h1 className="text-4xl font-bytesized text-foreground p-4">
-        Urban Pulse |{" "}
-        <span className="text-2xl text-foreground/50">City of Calgary</span>
-      </h1>
+      <Link href="/">
+        <h1 className="text-4xl font-bytesized text-foreground p-4">
+          Urban Pulse |{" "}
+          <span className="text-2xl text-foreground/50">City of Calgary</span>
+        </h1>
+      </Link>
       <div className="flex flex-row items-center justify-center gap-4 pr-4">
-        {links.map((link) =>
-          link.show && (typeof link.label === "string" ? (
-            <Button variant="ghost" size="lg" asChild key={link.href}>
-              <Link
-                href={link.href}
-                className={cn(
-                  pathname === link.href
-                    ? "text-primary"
-                    : "text-foreground/50",
-                  "hover:text-primary"
-                )}
-              >
+        {links.map(
+          (link) =>
+            link.show &&
+            (typeof link.label === "string" ? (
+              <Button variant="ghost" size="lg" asChild key={link.href}>
+                <Link
+                  href={link.href}
+                  className={cn(
+                    pathname === link.href
+                      ? "text-primary"
+                      : "text-foreground/50",
+                    "hover:text-primary"
+                  )}
+                >
+                  {link.label}
+                </Link>
+              </Button>
+            ) : (
+              <Link href={link.href} key={link.href}>
                 {link.label}
               </Link>
-            </Button>
-          ) : (
-            <Link href={link.href} key={link.href}>
-              {link.label}
-            </Link>
-          ))
+            ))
         )}
       </div>
     </nav>
