de9d1438556a9326c27eebc9e9965e9d2b7d82a2
Mohammad Al-Ahdal <mo@mohammad.dev>
2025-11-08 12:20:15 -0700

    add basics

---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer className="w-full flex items-center justify-center py-4 px-8 bg-gray-800 text-white">
+      <div className="text-sm">&copy; 2024 Urban Pulse. All rights reserved.</div>
+    </footer>
+  );
+}
