434876e1a140d9cb460efb4c04e457f234efd502
Mohammad Al-Ahdal <mo@mohammad.dev>
2025-11-08 12:35:00 -0700

    feat: integrate Mapbox GL and react-map-gl into the project
    - Added dependencies for Mapbox GL and react-map-gl in package.json and bun.lock.
    - Updated the Home component in page.tsx to include a Map component with initial view settings and Mapbox styles.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
+"use client";
+
 import Image from "next/image";
+import Map from 'react-map-gl/mapbox';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 export default function Home() {
   return (
     <main className="flex flex-1 flex-col items-center justify-center p-24">
-      dsadsa
+      <Map
+      mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
+      initialViewState={{
+        longitude: -122.4,
+        latitude: 37.8,
+        zoom: 14
+      }}
+      style={{width: "100vw", height: "100vh"}}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+    />
     </main>
   );
 }
