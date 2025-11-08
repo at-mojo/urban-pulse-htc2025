503e355a55a4ee23f215a5cb892a66978b4cfc28
Mohammad Al-Ahdal <mo@mohammad.dev>
2025-11-08 14:07:48 -0700

    style: update footer component styles for improved aesthetics and readability

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
-    <footer className="w-full flex items-center justify-center py-4 px-8 bg-gray-800 text-white">
-      <div className="text-sm">&copy; 2024 Urban Pulse. All rights reserved.</div>
+    <footer className="w-full flex items-center justify-center py-4 px-8 bg-background/50 backdrop-blur-lg z-50">
+      <div className="text-lg text-foreground/50 font-bytesized">&copy; 2024 Urban Pulse. All rights reserved.</div>
     </footer>
   );
 }
