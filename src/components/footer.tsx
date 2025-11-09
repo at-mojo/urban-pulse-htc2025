6c78e1cc699eb7a4ec29465eff57cbf3ee318641
Mohammad Al-Ahdal <mo@mohammad.dev>
2025-11-08 19:11:43 -0700

    fix: update footer text to include additional phrase for clarity

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <footer className="w-full flex items-center justify-center py-4 px-8 bg-background/50 backdrop-blur-lg z-50">
-      <div className="text-lg text-foreground/50 font-departure-mono">&copy; 2024 Urban Pulse. All rights reserved.</div>
+      <div className="text-lg text-foreground/50 font-departure-mono">&copy; 2024 Urban Pulse. Wee Woo. Wee Woo.</div>
     </footer>
   );
 }
