3d7765890f170f72506f06c7985924d570816922
Mohammad Al-Ahdal <mo@mohammad.dev>
2025-11-09 09:13:57 -0700

    jank.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -8,6 +8,7 @@ import type { Report } from "@prisma/client";
 import { getAllReports } from "@/report";
 import EventsTable from "@/components/events-table";
 import { ChevronsUpIcon, ChevronUpIcon, MinusIcon } from "lucide-react";
+import { useUser } from "@stackframe/stack";
 export default function Dashboard() {
   const [value, setValue] = useState("map");
   const [reports, setReports] = useState<Report[]>([]);
@@ -23,6 +24,8 @@ export default function Dashboard() {
     fetchReports();
   }, []);
 
+  const user = useUser();
+
   return (
     <main className="flex flex-1 flex-col items-center justify-center relative pt-[72px]">
       <TabView onChange={(value) => setValue(value)}>
@@ -36,6 +39,12 @@ export default function Dashboard() {
           name="Overview"
           value="overview"
           selected={value === "overview"}
+          onChange={(value) => setValue(value)}
+        />
+        <TabButton
+          name="My Reports"
+          value="my-reports"
+          selected={value === "my-reports"}
           onChange={(value) => setValue(value)}
         />
       </TabView>
@@ -72,6 +81,29 @@ export default function Dashboard() {
                 </div>
                 <div className="w-full bg-background rounded-xl p-4 border-2 border-border flex flex-row items-center gap-4">
                   <MinusIcon className="w-32 h-32" color="#0ea55f" /> <span className="text-2xl font-bold font-departure-mono">{reports.filter((report) => report.urgency === "LOW").length} Low Priority Reports</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+      {value === "my-reports" && (
+        <div className="w-full h-full relative flex flex-1">
+          <div className="w-full h-full bg-background rounded-md p-4">
+            <h1 className="text-3xl font-bold font-departure-mono">My Reports</h1>
+            <div className="w-full h-full flex flex-row gap-4 bg-background rounded-xl p-4 border-2 border-border mt-4">
+              <div className="flex-2 h-full">
+                <EventsTable reports={reports.filter((report) => report.userId === user?.id)} />
+              </div>
+              <div className="flex flex-col gap-4 flex-1 h-full">
+                <div className="w-full bg-background rounded-xl p-4 border-2 border-border flex flex-row items-center gap-4">
+                  <ChevronsUpIcon className="w-32 h-32" color="#e44d4d" /> <span className="text-2xl font-bold font-departure-mono">{reports.filter((report) => report.urgency === "HIGH" && report.userId === user?.id).length} High Priority Reports</span>
+                </div>
+                <div className="w-full bg-background rounded-xl p-4 border-2 border-border flex flex-row items-center gap-4">
+                  <ChevronUpIcon className="w-32 h-32" color="#f18227" /> <span className="text-2xl font-bold font-departure-mono">{reports.filter((report) => report.urgency === "MEDIUM" && report.userId === user?.id).length} Medium Priority Reports</span>
+                </div>
+                <div className="w-full bg-background rounded-xl p-4 border-2 border-border flex flex-row items-center gap-4">
+                  <MinusIcon className="w-32 h-32" color="#0ea55f" /> <span className="text-2xl font-bold font-departure-mono">{reports.filter((report) => report.urgency === "LOW" && report.userId === user?.id).length} Low Priority Reports</span>
                 </div>
               </div>
             </div>
