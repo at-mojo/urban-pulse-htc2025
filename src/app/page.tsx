02c24a0f64a456088678b1adca719349654c22b0
Mohammad Al-Ahdal <mo@mohammad.dev>
2025-11-08 19:38:33 -0700

    feat: implement scroll-based visibility for MouseIcon and add navigation button to Dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,50 @@
 "use client";
 
-import Image from "next/image";
+import { useEffect, useState } from "react";
+import { MouseIcon } from "lucide-react";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
+  const [isNotAtY0, setIsNotAtY0] = useState(false);
+
+  useEffect(() => {
+    const handler = () => {
+      if (window.scrollY !== 0) {
+        setIsNotAtY0(true);
+      } else {
+        setIsNotAtY0(false);
+      }
+    };
+    window.addEventListener("scroll", handler);
+    return () => window.removeEventListener("scroll", handler);
+  }, []);
+
   return (
     <main className="flex flex-1 flex-col items-center justify-center relative">
-      <Image
-        src="https://images.unsplash.com/photo-1727239122674-879c3157d491"
-        alt="Urban Pulse"
-        width={1000}
-        height={1000}
-      />
+      <div className="w-screen h-screen flex items-center justify-center relative">
+        <h1 className="text-5xl font-bold font-departure-mono text-foreground animate-pulse">
+          Urban Pulse
+        </h1>
+        <MouseIcon
+          className={cn(
+            "w-10 h-10 text-foreground absolute bottom-10 left-1/2 -translate-x-1/2 animate-bounce transition-opacity duration-300",
+            isNotAtY0 ? "opacity-0" : "opacity-100"
+          )}
+        />
+      </div>
+      <div className="w-screen h-screen flex items-center justify-center relative">
+        <Button
+          variant="ghost"
+          size="lg"
+          asChild
+        >
+          <Link href="/dashboard">
+            <h1>Dashboard</h1>
+          </Link>
+        </Button>
+      </div>
     </main>
   );
 }
